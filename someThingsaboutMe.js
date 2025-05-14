02389473191512af47a29b8c943ae837b8431bbe
SELECT-NAM801 <nathanmarshall8181@gmail.com>
2025-05-14 14:28:30 -0600

    About me

---
+++ someThingsaboutMe.js
@@ -0,0 +1,27 @@
+Hey my Name Is() {
+    Nate Marshall
+}
+
+I am an entry level Software QA looking to grow and learn more and more.
+I began my journey into tech at the beginning of 2025, after A Certifiction in: {
+        Software Quality Assurance,
+        from
+        MountainLand Technincal College
+            Lehi, Utah
+        In the heart of Silicon Slopes
+}
+
+I am enjoying my time doing it so far, and I look to expand my knowledge and take on new challenges
+
+    My skills are() {
+        Manual Testing
+        Software Testing
+        Test planning
+        SQL
+        JavaScript
+        Postman API
+        Bug finder
+}
+
+
+    I am working on this more and more
